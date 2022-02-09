52e972627816ab00991527e8ad001fb28d5d8027
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2022-02-09 12:39:41 -0500

    completed particles and now learning blender. Updating all cheatsheets

---
+++ admin/13-Blender/KeyboardShortcuts.js
@@ -0,0 +1,146 @@
+const Blender_KeyboardShortcuts = {
+  Numpad: {
+
+    ID_Class: 'NPC = NumpadCode',
+    // Template
+    ShortcutID: 'NPC00',
+    ShorcutName: "NumPad#",
+    KeyboardInput: "NUMPAD_#",
+    Function: '',
+    Notes: [
+
+    ],
+
+    // Switch Between Perspective View vs Orthographic View
+    ShortcutID: 'NPC05',
+    ShorcutName: "NumPad#5",
+    KeyboardInput: "NUMPAD_#5",
+    Function: 'Switch Between Perspective View vs Orthographic View',
+    Notes: [
+
+    ],
+
+
+    // Axis X, Y, and Z
+    ShortcutID: ['NPC01', 'NPC03', 'NPC07'],
+    ShorcutName: ["NumPad#1", "Numpad#3", "NumPad#7"],
+    KeyboardInput: ["NumPad#1", "Numpad#3", "NumPad#7"],
+    Function: 'x, y, z - axis view points,
+    Notes: [
+
+    ],
+
+    // Camera View Point / Perspective
+    ShortcutID: 'NPC00',
+    ShorcutName: "NumPad#0",
+    KeyboardInput: "NUMPAD_#0",
+    Function: 'Camera View Point / Perspective',
+    Notes: [
+
+    ],
+
+
+    // To focus the camera on an object
+    ShortcutID: 'NPC0(.)',
+    ShorcutName: "NumPad#(.)",
+    KeyboardInput: "Select with Left Mouse, then NUMPAD_#(.)",
+    Function: '',
+    Notes: [
+
+    ],
+  },
+
+  Selecting: {
+
+    ID_Class: 'SEL = Selection',
+
+
+    // Template
+    ShortcutID: 'SEL00',
+    ShorcutName: "SEL_",
+    UserInput: "SEL_",
+    Function: '',
+    Notes: [],
+
+    // Selecting
+    ShortcutID: 'SEL01',
+    ShorcutName: "SEL_Item",
+    UserInput: "SEL + Left Mouse",
+    AdditionalInputs: {
+      SelectMultiple: ["SEL_Multiple", 'Shift + Left Click'],
+      SelectAll: ["SEL_ALL", 'Key="A"'],
+      SelectWithRectangle: ["SEL_Rect", 'Key="B"'],
+    },
+    Function: '',
+    Notes: [],
+  },
+
+
+  Object_Shortcuts: {
+    ID_Class: 'CRSI = CreationSimulation',
+
+    // Template
+    ShortcutID: 'CRSI00',
+    ShorcutName: "CRSI_",
+    UserInput: "",
+    Function: '',
+    Notes: [],
+
+
+    // Adding an object 
+    ShortcutID: 'CRSI_01',
+    ShorcutName: "CRSI_Create",
+    UserInput: "Shift + A",
+    Function: 'Opens up a menu to drop more objects.',
+    Notes: [],
+
+    // Deleting an object 
+    ShortcutID: 'CRSI_02',
+    ShorcutName: "CRSI_Delete",
+    UserInput: "X",
+    Function: 'Prompt for deletion of selection',
+    Notes: [],
+
+    // Hiding a single object
+    ShortcutID: 'CRSI_03',
+    ShorcutName: "CRSI_HidingSingleObject",
+    UserInput: "H",
+    Function: 'Hiding a single object',
+    Notes: [],
+
+    // Hiding all objects
+    ShortcutID: 'CRSI_04',
+    ShorcutName: "CRSI_HidingMultipleObjects",
+    UserInput: "Shift + H",
+    Function: 'Hiding all objects',
+    Notes: [],
+
+
+    // # Transforming objects
+    // { Position, Rotation, and Scale }
+
+    // Position Change
+    ShortcutID: 'CRSI_05',
+    ShorcutName: "CRSI_Position",
+    UserInput: "G",
+    Function: 'Makes user able to move object',
+    Notes: [],
+
+    // Rotation Change
+    ShortcutID: 'CRSI_06',
+    ShorcutName: "CRSI_Rotation",
+    UserInput: "R",
+    Function: 'Makes user able to rotate object',
+    Notes: [],
+
+    // Scale Change
+    ShortcutID: 'CRSI_07',
+    ShorcutName: "CRSI_Scale",
+    UserInput: "S",
+    Function: 'Makes user able to scale object',
+    Notes: [],
+
+  },
+
+
+}
---
+++ admin/13-Blender/UI_Learning.js
@@ -0,0 +1,62 @@
+const ViewingTheScene = {
+  DifferentMovements: {
+    Movement00: {
+      MovementName: "",
+      Shortcut: "",
+      Notes: [],
+    },
+
+    Movement01: {
+      MovementName: "Orbit",
+      Shortcut: "MOUSEWHEEL - Pressed Down",
+      Notes: [
+        {
+          Function: 'To move view around a single target',
+        },
+      ],
+    },
+
+    Movement02: {
+      MovementName: ["Truck", "Track", "Pan (incorrect)"],
+      Shortcut: "Shift + MOUSEWHEEL Button + Mouse-Movement-Left or Right",
+      Notes: [
+        {
+          Function: 'To move view right or left',
+        },
+      ],
+    },
+
+    Movement03: {
+      MovementName: "Pedestal",
+      Shortcut: "Shift + MOUSEWHEL Button + Mouse-Movement-Up or Down",
+      Notes: [
+        {
+          Function: 'To move view upwards or downwards',
+        },
+      ],
+    },
+
+    Movement04: {
+      MovementName: "Dolly",
+      Shortcut: "MOUSEWHEEL (Roll) ",
+      Notes: [
+        {
+          Function: 'To zoom / scale in or out.',
+        },
+      ],
+    },
+
+
+    Movement05: {
+      MovementName: "Super Dolly / Resolve Zoom Limit Movement",
+      Shortcut: "Shift + Ctrl + MIDDLEMOUSE",
+      Notes: [
+        {
+          Function: 'To zoom scale in or out and even beyond 0',
+        },
+      ],
+    },
+
+
+  }
+}
